2e95588d979a9a9402c0b92c4d9cf60c0ed33529
Jakub-Ignatowicz <kuba_ign@proton.me>
2023-11-27 13:35:52 +0100

    Implemented printing function for searching algorithms

--- main.go
+++ main.go
@@ -9,6 +9,27 @@ import (
 )
 
 type sortingFunc func(arr []int) uint64
+type searchingFunc func(arr []int, valueToFind int) (uint64, int)
+
+func printSearchingAlgorithm(searchFunc searchingFunc, algName string) {
+	fmt.Println("\n---------------------")
+	fmt.Println(algName)
+	fmt.Println("---------------------\n")
+
+	array := []int{2, 3, 5, 9, 12, 14, 19, 23, 27, 29, 34, 38, 41, 45, 49, 50, 56, 61, 64, 67, 71, 72, 76, 78, 83, 88, 89, 90, 94, 98}
+
+	start := time.Now()
+	iterationCount, index := searchFunc(array, 29)
+	if index == -1 {
+		fmt.Println("Value hasn't been found in this array")
+	}
+	end := time.Now()
+	duration := end.Sub(start)
+
+	iterationCountStr := strconv.FormatUint(iterationCount, 10)
+
+	fmt.Printf("Index of value 29 was found with %s in %s iterations and %.2f nanoseconds\n", algName, iterationCountStr, float64(duration.Nanoseconds()))
+}
 
 func printSortingAlgorithm(sortFunc sortingFunc, algName string) {
 	fmt.Println("\n---------------------")
