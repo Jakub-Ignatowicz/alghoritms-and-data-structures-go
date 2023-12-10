7e691c8de6e2fb42e602678dc4f47dac3c5d7360
Jakub-Ignatowicz <kuba_ign@proton.me>
2023-12-10 20:07:35 +0100

    Fixed selection sort

--- sorting/selectionSort.go
+++ sorting/selectionSort.go
@@ -1,25 +1,19 @@
 package sorting
 
 func SelectionSort(arr []int) uint64 {
+	n := len(arr)
 	var iterations uint64 = 0
-	len := len(arr)
-	if len <= 1 {
-		return 0
-	}
-	for i := 0; i < len-1; i++ {
-		var lowest int
-		var lowestIndex int
-		for j := i; j < len; j++ {
-			iterations++
-			if j == i {
-				lowest = arr[j]
-				lowestIndex = j
-			} else if arr[j] < lowest {
-				lowest, arr[j] = arr[j], lowest
-				lowestIndex, j = j, lowestIndex
+
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
 			}
+			iterations++
 		}
-		arr[i] = lowest
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
 	}
+
 	return iterations
 }
