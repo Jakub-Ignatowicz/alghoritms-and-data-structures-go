7786752add5795ecea62c199cb31b893e0188569
Jakub-Ignatowicz <kuba_ign@proton.me>
2023-12-11 17:01:13 +0100

    implemented memoization in dynamic programming

---
+++ dynamic_programming/memoization/fibonacci.go
@@ -0,0 +1,20 @@
+package memoization
+
+var fibCache = make(map[int]int)
+
+func Fibonacci(n int) int {
+
+	if result, found := fibCache[n]; found {
+		return result
+	}
+
+	if n <= 1 {
+		return n
+	}
+
+	fibValue := Fibonacci(n-1) + Fibonacci(n-2)
+
+	fibCache[n] = fibValue
+
+	return fibValue
+}
